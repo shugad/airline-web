9381ca8d8437ba13d5f5c65649b0eecc98e0113c
shugad <emilycarter20001@gmail.com>
2022-02-09 16:04:23 +0300

    First commit

---
+++ main/handler/preprocessing.py
@@ -0,0 +1,100 @@
+import numpy as np
+import pickle
+import sklearn
+import catboost
+from scipy.special import inv_boxcox
+
+ALPHA = 0.30550104689624275
+
+dict_groups_airlines = {
+    'multiple carriers premium economy': 'premium', 'jet airways business': 'premium',
+    'jet airways': 'premium', 'vistara': 'medium', 'vistara premium economy': 'medium',
+    'goair': 'medium', 'multiple carriers': 'medium', 'air india': 'medium',
+    'trujet': 'low-cost', 'spicejet': 'low-cost', 'indigo': 'low-cost', 'air asia': 'low-cost'
+}
+
+dict_airlines_enc = {'air asia': 0, 'air india': 1, 'goair': 2, 'indigo': 3,
+                     'jet airways': 4, 'jet airways business': 5, 'multiple carriers': 6,
+                     'multiple carriers premium economy': 7, 'spicejet': 8,
+                     'trujet': 9, 'vistara': 10, 'vistara premium economy': 11}
+
+dict_groups_airlines_enc = {'low-cost': 0, 'medium': 1, 'premium': 2}
+
+dict_time_of_day_enc = {'Early Morning': 0, 'Morning': 1,
+                        'Noon': 2, 'Eve': 3, 'Night': 4, 'Late Night': 5}
+
+dict_season_enc = {'spring': '0', 'summer': '1', 'fall': '2', 'winter': '3'}
+
+
+def get_group_airline(value):
+    value = value.lower()
+    dictionary = dict_groups_airlines
+    category = dictionary[value]
+    return encode_group_airline(category)
+
+
+def encode_group_airline(value):
+    value = value.lower()
+    dictionary = dict_groups_airlines_enc
+    return dictionary[value]
+
+
+def encode_radio_1(value):
+    if value == 'No':
+        return 0
+    else:
+        return 1
+
+
+def encode_radio(value):
+    if value == 'No':
+        return 0
+    else:
+        return 1
+
+
+def encode_airline(value):
+    value = value.lower()
+    dictionary = dict_airlines_enc
+    return dictionary[value]
+
+
+def encode_time_of_day(value):
+    dictionary = dict_time_of_day_enc
+    return dictionary[value]
+
+
+def encode_season(value):
+    value = get_season(value)
+    dictionary = dict_season_enc
+    return dictionary[value]
+
+
+# function for month extraction
+def get_season(month):
+    if month in [12, 1, 2]:
+        return "winter"
+    elif month in [3, 4, 5]:
+        return "spring"
+    elif month in [6, 7, 8]:
+        return "summer"
+    else:
+        return "fall"
+
+
+def get_duration(hours, minutes):
+    return int(hours) * 60 + int(minutes)
+
+
+def get_prediction(model, x):
+    if model == 'gbr':
+        model_loaded = pickle.load(open(r'main\models\gbr.sav', 'rb'))
+    elif model == 'catboost':
+        model_loaded = pickle.load(open(r'main\models\catboost.sav', 'rb'))
+    pred = model_loaded.predict([x])
+    return np.round(inv_boxcox(pred, ALPHA)[0], 2)
+
+
+""""def get_final_prediction(X):
+    return np.round((get_prediction('gbr', X) + get_prediction('catboost', X))/2, 2)"""
+
---
+++ main/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from datetime import datetime
+from .handler.preprocessing import get_group_airline, encode_airline, \
+    encode_season, encode_time_of_day, encode_radio, get_duration, get_prediction
+
+
+def home(request):
+    if request.POST:
+        airline = request.POST['user_airline']
+        total_stops = request.POST['user_total_stops']
+        date = request.POST['user_date']
+        month = datetime.strptime(request.POST['user_date'], "%Y-%m-%d").month
+        day = datetime.strptime(request.POST['user_date'], "%Y-%m-%d").day
+        season = encode_season(month)
+        airline_group = get_group_airline(airline)
+        airline_encoded = encode_airline(airline)
+        hours = request.POST['user_hours']
+        minutes = request.POST['user_minutes']
+        duration = get_duration(hours, minutes)
+        period_of_day = request.POST['user_duration_time']
+        period_of_day_enc = encode_time_of_day(period_of_day)
+        user_baggage = encode_radio(request.POST['user_baggage'])
+        user_layover = encode_radio(request.POST['user_long_layover'])
+        user_business = encode_radio(request.POST['user_business_class'])
+        user_change = encode_radio(request.POST['user_change_airports'])
+        user_meal = encode_radio(request.POST['user_meal'])
+        X = []
+        X.append(airline_encoded)
+        X.append(total_stops)
+        X.append(month)
+        X.append(day)
+        X.append(season)
+        X.append(period_of_day_enc)
+        X.append(user_layover)
+        X.append(user_business)
+        X.append(user_change)
+        X.append(user_meal)
+        X.append(user_baggage)
+        X.append(airline_group)
+        X.append(duration)
+
+        predicted = get_prediction('gbr', X)
+
+        context = {'airline': airline, 'date': date,
+                   'hours': hours, 'minutes': minutes,
+                   'X': X, 'predicted': predicted}
+
+        return render(request, 'main/output.html', context)
+    return render(request, 'main/input.html')
+
